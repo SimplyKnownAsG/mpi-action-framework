418a06e416750e2a18888a073f0e252d7bf7b10a
g <gtmalmgren@gmail.com>
2016-09-13 22:52:15 -0700

    Added set_up method, so that someone could use it for taking stuff from the Context

--- maf/actions/Action.cpp
+++ maf/actions/Action.cpp
@@ -14,12 +14,17 @@ namespace maf {
         return;
     }
 
+    void Action::set_up() {
+        return;
+    }
+
     void Action::tear_down() {
         return;
     }
 
     void Action::start(std::shared_ptr<Context> context) {
         this->context = context;
+        this->set_up();
         this->run();
         this->tear_down();
     }
